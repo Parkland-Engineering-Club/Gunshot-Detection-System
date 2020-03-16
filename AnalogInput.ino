651af7a782f001c51027717a01b467807b7bfa78
jd2012 <jdkwyo@gmail.com>
2020-03-15 21:14:08 -0700

    Update AnalogInput.ino
    fixed the serial output format

--- AnalogInput.ino
+++ AnalogInput.ino
@@ -1,11 +1,9 @@
 /*
   Analog Input
-
   Demonstrates analog input by reading an analog sensor on analog pin 0 and
   turning on and off a light emitting diode(LED) connected to digital pin 13.
   The amount of time the LED will be on and off depends on the value obtained
   by analogRead().
-
   The circuit:
   - potentiometer
     center pin of the potentiometer to the analog input 0
@@ -14,16 +12,12 @@
   - LED
     anode (long leg) attached to digital output 13
     cathode (short leg) attached to ground
-
   - Note: because most Arduinos have a built-in LED attached to pin 13 on the
     board, the LED is optional.
-
   created by David Cuartielles
   modified 30 Aug 2011
   By Tom Igoe
-
   This example code is in the public domain.
-
   http://www.arduino.cc/en/Tutorial/AnalogInput
 */
 #include <LiquidCrystal.h> //LCD library
@@ -44,21 +38,32 @@ void setup() {
 }
 
 void loop() {
-  // read the value from the sensor:
-  trigger1 = analogRead(t1Pin);
-  trigger2 = analogRead(t2Pin);
-  float v1 = trigger1 * (5.0 / 1023.0);
-  float v2 = trigger2 * (5.0 / 1023.0);
-  Serial.print("A0 Analog,A1 Analog"); //prints shot count to the serial
-  Serial.println(trigger1" , "trigger2);
-  lcd.setCursor(0, 0);
-  lcd.print("A0"); //diplay alert on LCD
-  lcd.setCursor(4, 0);
-  lcd.print(trigger1);
-  lcd.setCursor(0, 1);
-  lcd.print("A1"); //diplay alert on LCD
-  lcd.setCursor(4, 1);
-  lcd.print(trigger2);
   
-  delay(500); //delay .5 secs
+  Serial.print("A0 Analog");
+  Serial.print("\t\t");
+  Serial.println("A1 Analog");
+  Serial.print("");
+  Serial.print("\t\t");
+  Serial.println("A1 Analog");//prints shot count to the serial
+  int i = 0;
+  while(i < 26) {
+    i++;
+    // read the value from the sensor:
+    trigger1 = analogRead(t1Pin);
+    trigger2 = analogRead(t2Pin);
+    float v1 = trigger1 * (5.0 / 1023.0);
+    float v2 = trigger2 * (5.0 / 1023.0);
+    Serial.print(trigger1);
+    Serial.print("\t\t\t");
+    Serial.println(trigger2);
+    lcd.setCursor(0, 0);
+    lcd.print("A0"); //diplay alert on LCD
+    lcd.setCursor(4, 0);
+    lcd.print(trigger1);
+    lcd.setCursor(0, 1);
+    lcd.print("A1"); //diplay alert on LCD
+    lcd.setCursor(4, 1);
+    lcd.print(trigger2);
+    delay(500); //delay .5 secs
+  }
 }
